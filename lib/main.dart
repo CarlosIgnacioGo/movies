6edcdac7c91955afa123a9a2197d71b24b3067fb
Carlos Gomez <carlos221519@gmail.com>
2019-06-01 13:21:36 -0400

    Swiper package agregado y configuracion basica del mismo

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
  
+import 'package:movies/src/pages/home_page.dart';
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Películas',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => HomePage(),
+      },
     );
   }
 }
